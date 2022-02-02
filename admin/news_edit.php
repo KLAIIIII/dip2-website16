4a3c5e1e869d41f2b5e25361924a0a7fba0c9606
KLAIIIII <guyyg03546@gmail.com>
2022-02-02 10:59:20 +0700

    Update

---
+++ admin/news_edit.php
@@ -0,0 +1,23 @@
+<?php include("security.php"); ?>
+<?php include("includes/header.php"); ?>
+<?php include("includes/sidebar.php"); ?>
+<?php include("includes/topbar.php"); ?>
+<?php include("config/dbcon.php"); ?>
+
+<div class="container">
+
+<div class="card shadow mb-4">
+  <div class="card-header py-3">
+    <h5 class="font-weight-bold text-info">Admin Edit News</h5>
+  </div>
+  <div class="card-body">
+      
+  </div>
+</div>
+
+</div>
+
+
+<?php include("includes/footer.php") ?>
+<?php include("includes/logoutmodal.php") ?>
+<?php include("includes/scripts.php") ?>
