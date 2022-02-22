1c6bafd5e22d4ddd2d28f941bcbed0430d380eb2
KLAIIIII <guyyg03546@gmail.com>
2022-02-22 14:57:19 +0700

    update

--- admin/faculty.php
+++ admin/faculty.php
@@ -4,17 +4,32 @@
 <?php include("includes/topbar.php"); ?>
 <?php include("config/dbcon.php"); ?>
 
-<div class="container shadow">
-    <div class="card">
-        <div class="card-body">
-            <h1>Hello!</h1>
-        </div>
+<div class="container-fluid shadow px-3 py-5">
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
     </div>
-
-    <button class="btn btn-primary">ME</button>
-    <button class="btn btn-success">ME2</button>
-    <button class="btn btn-info">ME3</button>
-
+  </div>
+</div>
 </div>
 
 <?php include("includes/footer.php") ?>
