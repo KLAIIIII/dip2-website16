a16e660f3c437f7440ed084cba47d6ecbedc926b
KLAIIIII <guyyg03546@gmail.com>
2022-02-22 14:19:22 +0700

    Update

--- admin/faculty.php
+++ admin/faculty.php
@@ -4,7 +4,18 @@
 <?php include("includes/topbar.php"); ?>
 <?php include("config/dbcon.php"); ?>
 
-Hello faculty
+<div class="container shadow">
+    <div class="card">
+        <div class="card-body">
+            <h1>Hello!</h1>
+        </div>
+    </div>
+
+    <button class="btn btn-primary">ME</button>
+    <button class="btn btn-success">ME2</button>
+    <button class="btn btn-info">ME3</button>
+
+</div>
 
 <?php include("includes/footer.php") ?>
 <?php include("includes/logoutmodal.php") ?>
